MichaelDeets/corectrl

commit 36dc5c6556ec323b5a478dc89f8bee5177817e9d

Add CPU usage sensor
See !38 for more details.

src/core/components/cpuutils.h
@@ -19,5 +19,30 @@ std::optional<std::string>
 parseProcCpuInfo(std::vector<std::string> const &procCpuInfoLines, int cpuId,
                  std::string_view target);
 
+/// CPU Stat captured from /proc/stat.
+struct Stat
+{
+  std::uint64_t user;
+  std::uint64_t nice;
+  std::uint64_t system;
+  std::uint64_t idle;
+  std::uint64_t ioWait;
+  std::uint64_t irq;
+  std::uint64_t softIrq;
+  std::uint64_t steal;
+  std::uint64_t total;
+};
+
+/// Returns the aggregate CPU Stat from /proc/stat.
+/// @param procStatLines /proc/stat data.
+/// @returns CPU stat or none when the CPU stat cannot be parsed from the input data.
+std::optional<Stat> parseProcStat(std::vector<std::string> const &procStatLines);
+
+/// Computes the used CPU in a time interval defined by statT0 and statT1.
+/// @param statT0 CPU stat captured in time 0.
+/// @param statT1 CPU stat captured in time 1 (some time after statT0).
+/// @returns percentage of the CPU used in the time interval.
+unsigned int computeCPUUsage(Stat const &statT0, Stat const &statT1);
+
 } // namespace CPU
 } // namespace Utils

src/CMakeLists.txt
@@ -196,6 +196,7 @@ list(APPEND SENSORS_SRC
   core/components/sensors/amd/fanspeedperc.cpp
   core/components/sensors/cpusensorprovider.cpp
   core/components/sensors/cpu/cpufreqpack.cpp
+  core/components/sensors/cpu/cpuusage.cpp
 )
 
 list(APPEND UI_COMPONENTS_SRC
@@ -221,6 +222,7 @@ list(APPEND UI_COMPONENTS_SRC
   core/components/sensors/amd/fanspeedrpmgraphitem.cpp
   core/components/sensors/amd/fanspeedpercgraphitem.cpp
   core/components/sensors/cpu/cpufreqpackgraphitem.cpp
+  core/components/sensors/cpu/cpuusagegraphitem.cpp
   core/components/controls/controlmodeqmlitem.cpp
   core/components/controls/controlgroupqmlitem.cpp
   core/components/controls/noopqmlitem.cpp

src/core/components/cpuutils.cpp
@@ -6,6 +6,7 @@
 #include "common/stringutils.h"
 #include <algorithm>
 #include <iterator>
+#include <regex>
 
 namespace Utils {
 namespace CPU {
@@ -47,6 +48,51 @@ parseProcCpuInfo(std::vector<std::string> const &procCpuInfoLines, int cpuId,
   }
 
   return {};
+}
+
+std::optional<Stat> parseProcStat(std::vector<std::string> const &procStatLines)
+{
+  // CPU aggregate line columns:
+  //   cpu user nice system idle io_wait irq soft_irq steal guest guest_nice
+  //
+  // NOTE guest and guest_nice values are already included into user and nice.
+  // https://github.com/torvalds/linux/blob/master/kernel/sched/cputime.c#L143?h=5.0
+  static std::regex const regex(
+      R"(^cpu\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+\d+\s+\d+\s*$)",
+      std::regex::icase);
+
+  for (auto &line : procStatLines) {
+    std::smatch result;
+    if (std::regex_match(line, result, regex)) {
+      std::uint64_t user{0}, nice{0}, system{0}, idle{0}, ioWait{0}, irq{0},
+          softIrq{0}, steal{0}, total{0};
+
+      if (Utils::String::toNumber<std::uint64_t>(user, result[1]) &&
+          Utils::String::toNumber<std::uint64_t>(nice, result[2]) &&
+          Utils::String::toNumber<std::uint64_t>(system, result[3]) &&
+          Utils::String::toNumber<std::uint64_t>(idle, result[4]) &&
+          Utils::String::toNumber<std::uint64_t>(ioWait, result[5]) &&
+          Utils::String::toNumber<std::uint64_t>(irq, result[6]) &&
+          Utils::String::toNumber<std::uint64_t>(softIrq, result[7]) &&
+          Utils::String::toNumber<std::uint64_t>(steal, result[8])) {
+
+        total = user + nice + system + idle + ioWait + irq + softIrq + steal;
+        return Stat{user, nice,    system, idle, ioWait,
+                    irq,  softIrq, steal,  total};
+      }
+    }
+  }
+
+  return {};
+}
+
+unsigned int computeCPUUsage(Stat const &statT0, Stat const &statT1)
+{
+  auto total = statT1.total - statT0.total;
+  auto idle = (statT1.idle + statT1.ioWait) - (statT0.idle + statT0.ioWait);
+  auto used = total - idle;
+
+  return (100.0 * used) / total;
 }
 
 } // namespace CPU

src/core/components/sensors/cpu/cpuusage.cpp
@@ -1,0 +1,110 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright 2022 Milan Cermak <krysmanta@post.cz>
+// Copyright 2023 Juan Palacios <jpalaciosdev@gmail.com>
+
+#include "cpuusage.h"
+
+#include "../cpusensorprovider.h"
+#include "../graphitemprofilepart.h"
+#include "../graphitemxmlparser.h"
+#include "../sensor.h"
+#include "core/components/cpuutils.h"
+#include "core/idatasource.h"
+#include "core/info/icpuinfo.h"
+#include "core/iprofilepart.h"
+#include "core/iprofilepartxmlparser.h"
+#include "core/profilepartprovider.h"
+#include "core/profilepartxmlparserprovider.h"
+#include "core/sysfsdatasource.h"
+#include <algorithm>
+#include <filesystem>
+#include <memory>
+#include <optional>
+#include <string>
+#include <units.h>
+#include <utility>
+#include <vector>
+
+namespace CPUUsage {
+
+class CPUUsageDataSource : public IDataSource<unsigned int>
+{
+ public:
+  CPUUsageDataSource() noexcept
+  : procStatDataSource_("/proc/stat")
+  {
+  }
+
+  std::string source() const override
+  {
+    return procStatDataSource_.source();
+  }
+
+  bool read(unsigned int &usedCPU) override
+  {
+    if (procStatDataSource_.read(rawData_)) {
+
+      auto stat = Utils::CPU::parseProcStat(rawData_);
+      rawData_.clear();
+
+      if (stat.has_value()) {
+
+        if (prevStat_.has_value())
+          usedCPU = Utils::CPU::computeCPUUsage(*prevStat_, *stat);
+        else
+          usedCPU = 0;
+
+        prevStat_.swap(stat);
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+ private:
+  SysFSDataSource<std::vector<std::string>> procStatDataSource_;
+  std::vector<std::string> rawData_;
+  std::optional<Utils::CPU::Stat> prevStat_;
+};
+
+class Provider final : public ICPUSensorProvider::IProvider
+{
+ public:
+  std::vector<std::unique_ptr<ISensor>>
+  provideCPUSensors(ICPUInfo const &, ISWInfo const &) const override
+  {
+    std::vector<std::unique_ptr<ISensor>> sensors;
+
+    std::vector<std::unique_ptr<IDataSource<unsigned int>>> dataSources;
+    dataSources.emplace_back(std::make_unique<CPUUsageDataSource>());
+
+    sensors.emplace_back(
+        std::make_unique<Sensor<units::dimensionless::scalar_t, unsigned int>>(
+            CPUUsage::ItemID, std::move(dataSources),
+            std::make_pair(units::dimensionless::scalar_t(0),
+                           units::dimensionless::scalar_t(100))));
+
+    return sensors;
+  }
+};
+
+static bool register_()
+{
+  CPUSensorProvider::registerProvider(std::make_unique<CPUUsage::Provider>());
+
+  ProfilePartProvider::registerProvider(CPUUsage::ItemID, []() {
+    return std::make_unique<GraphItemProfilePart>(CPUUsage::ItemID,
+                                                  "yellowgreen");
+  });
+
+  ProfilePartXMLParserProvider::registerProvider(CPUUsage::ItemID, []() {
+    return std::make_unique<GraphItemXMLParser>(CPUUsage::ItemID);
+  });
+
+  return true;
+}
+
+static bool const registered_ = register_();
+
+} // namespace CPUUsage

src/core/components/sensors/cpu/cpuusage.h
@@ -1,0 +1,13 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright 2022 Milan Cermak <krysmanta@post.cz>
+
+#pragma once
+
+#include <string_view>
+
+/// A sensor that monitors the CPU usage.
+namespace CPUUsage {
+
+static constexpr std::string_view ItemID{"CPU_USAGE"};
+
+} // namespace CPUUsage

src/core/components/sensors/cpu/cpuusagegraphitem.cpp
@@ -1,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright 2022 Milan Cermak <krysmanta@post.cz>
+
+#include "../sensorgraphitem.h"
+#include "core/qmlcomponentregistry.h"
+#include "cpuusage.h"
+#include <QtGlobal>
+#include <units.h>
+
+namespace CPUUsage {
+
+bool const registered_ = QMLComponentRegistry::addQuickItemProvider(
+    CPUUsage::ItemID, []() {
+      return new SensorGraphItem<units::dimensionless::scalar_t, unsigned int>(
+          CPUUsage::ItemID, "%");
+    });
+
+char const *const trStrings[] = {
+    QT_TRANSLATE_NOOP("SensorGraph", "CPU_USAGE"),
+};
+
+} // namespace CPUUsage

src/translations/lang_bg_BG.ts
@@ -723,6 +723,11 @@ Check the permissions of the destination file and directory.</source>
         <source>AMD_GPU_MEMORY_TEMP</source>
         <translation>Температура на паметта</translation>
     </message>
+    <message>
+        <location filename="../core/components/sensors/cpu/cpuusagegraphitem.cpp" line="19"/>
+        <source>CPU_USAGE</source>
+        <translation>Използване</translation>
+    </message>
 </context>
 <context>
     <name>SettingsDialog</name>

src/translations/lang_ca_ES.ts
@@ -723,6 +723,11 @@ Comprova els permisos del fitxer i del directori de destí.</translation>
         <source>AMD_GPU_MEMORY_TEMP</source>
         <translation>Temperatura (memòria)</translation>
     </message>
+    <message>
+        <location filename="../core/components/sensors/cpu/cpuusagegraphitem.cpp" line="19"/>
+        <source>CPU_USAGE</source>
+        <translation>Ús</translation>
+    </message>
 </context>
 <context>
     <name>SettingsDialog</name>

commit 6ce3a81086aeb5961e81a0735e7f1468377afa42

Fix compilation issue
Introduced by 36dc5c6, due to a missing include.

src/core/components/cpuutils.h
@@ -3,6 +3,7 @@
 
 #pragma once
 
+#include <cstdint>
 #include <optional>
 #include <string>
 #include <string_view>